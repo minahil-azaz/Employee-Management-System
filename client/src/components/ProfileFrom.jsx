156b847af7b25c8e1a804d228cb91f0ae8693c70
minahil azaz <99528192+minahil-azaz@users.noreply.github.com>
2026-04-28 17:19:59 +0500

    setting from working

--- client/src/components/ProfileFrom.jsx
+++ client/src/components/ProfileFrom.jsx
@@ -16,10 +16,17 @@ const ProfileFrom = () => {
         <User className="w-5 h-5 text-slate-400"/>public Profile
       </h2>
       {error && (
-        <div className='flex items-start mb-6 text-sm border rounded bg-rose-50 text-rose-200 '>
-          <div/>
+        <div className='flex items-start gap-3 mb-6 text-sm border rounded bg-rose-50 text-rose-200'>
+          <div className='w-1.5 h-1.5 rounded-full bg-rose-500 shrink-0 mt-1.5 '/>
             
-          
+          {error}
+        </div>
+      )}
+      {message && (
+        <div className='flex items-start gap-3 mb-6 text-sm border rounded-xl bg-emerald-50 text-emerald-700 border-emerald-200' p-4>
+          <div className='w-1.5 h-1.5 rounded-full bg-emerald-500 shrink-0 mt-1.5 '/>
+            
+          {message}
         </div>
       )}
 
