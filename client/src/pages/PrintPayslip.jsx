c992cff616d5d5f524dc5162596ab6df76b14251
minahil azaz <99528192+minahil-azaz@users.noreply.github.com>
2026-04-29 21:17:50 +0500

    working in payslip print

--- client/src/pages/PrintPayslip.jsx
+++ client/src/pages/PrintPayslip.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyEmployeeData } from "../data/dymmyPaySlipData";
+import { Loader } from 'lucide-react';
+import { format } from 'date-fns';
 
 const Printpayslip = () => {
+  const{id} = useParams();
+  const[paySlip,setPaySlip] = useState(null)
+  const[loading,setLoading] = useState(true)
+
+
+  useEffect(() =>{
+    setPaySlip(dymmyPaySlipData.find((slip) =>slip._id === id))
+    setTimeout(()=>{
+      setLoading(false)
+    },1000)
+  },[id])
+
+  if (loading) return <Loading />
+  if(!paySlip) return <p className='py-12 text-center text-slate-400'>Payslip not found</p>
+
+
   return (
-    <div>printpayslip</div>
+    <div></div>
   )
 }
 
