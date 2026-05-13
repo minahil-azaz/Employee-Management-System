61be710d16aea46573e29849ff26be3878f892aa
Minahil Azaz <99528192+minahil-azaz@users.noreply.github.com>
2026-05-13 18:01:00 +0500

    Update employeeRoutes.js

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -1,9 +1,9 @@
 import {Router} from 'express';
 
-import { getEmployees } from '../controllers/EmployeeController.js';
-import { createEmployee } from '../controllers/EmployeeController.js';
-import { updateEmployee } from '../controllers/EmployeeController.js';
-import { deleteEmployee } from '../controllers/EmployeeController.js';
+import { getEmployees } from '../controllers/employeeController.js';
+import { createEmployee } from '../controllers/employeeController.js';
+import { updateEmployee } from '../controllers/employeeController.js';
+import { deleteEmployee } from '../controllers/employeeController.js';
 import { protect,protectAdmin } from '../middleware/authMiddleware.js';
 
 
